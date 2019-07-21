359339a5ce7aaab0fa5c8547b101b57fc0724b59
yanduanyin <1097720997@qq.com>
2019-07-22 00:58:53 +0800

    仿豆瓣官网页面

--- item-douban/main.js
+++ item-douban/main.js
@@ -1,9 +1,10 @@
-const login= document.querySelectorAll('.login-in-on');
+const login = document.querySelectorAll('.login-in-on');
 // 获取页面
-const tabs= document.querySelectorAll('.li-table');
+const tabs = document.querySelectorAll('.li-table');
 // 获取li标签
+const taba = document.querySelectorAll('.not-recrived');
 // console.log(login);
-for(let i=0;i<tabs.length;i++){
+for(var i=0;i<tabs.length;i++){
     tabs[i].addEventListener('click',function(){
         showtab.call(this);
         // this由window变为tabs的内容
@@ -11,7 +12,7 @@ for(let i=0;i<tabs.length;i++){
 }
 function showtab(){
     console.log(this);
-    for(let j=0;j<login.length;j++){
+    for(var j = 0;j<login.length;j++){
         if(tabs[j]=== this){
             login[j].classList.add('show');
             tabs[j].classList.add('tab');         
@@ -22,6 +23,21 @@ function showtab(){
     }
 }
 
+for(var k = 0;k < taba.length;k++){
+    taba[k].addEventListener('click',function(){
+        ashow.call(this);
+    })
+}
+
+function ashow(){
+    for(var m = 0;m<login.length;m++)
+    console.log(this);
+    if(taba[m] === this){
+        login[m].classList.remove('show'); 
+    }else{
+        login[m].classList.add('show');
+    }
+}
 // 第一种方法
 // function showtab(){
 //     console.log(this);
