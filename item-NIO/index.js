c4a6b8096a4ca7685140116434b9fce2830e7a48
yanduanyin <1097720997@qq.com>
2019-08-07 16:13:45 +0800

    NIO-界面基本完成，欠懒加载

--- item-NIO/index.js
+++ item-NIO/index.js
@@ -27,94 +27,123 @@ for (let j = 0; j < lineList.length; j++) {
         spanLine[j].classList.remove('spanline')
     })
 }
-//轮播图js部分
-/*获取HTML中的对象*/
-var parent = document.getElementById("parent");
-var img_ul = document.getElementById("img_ul");
-var litCir_ul = document.getElementById("litCir_ul");
-// var buttons = document.getElementById("buttons");
-var cLis =litCir_ul.children;         //children 属性返回元素的子元素的集合
-//定义一些变量
-var len = img_ul.children.length;     //图片张数
-var width = parent.offsetWidth;       //每张图片的宽度
-var rate = 50;                        //一张图片的切换速度， 单位为px
-var times = 1;                        //切换速度的倍率
-var gap = 2000;                       //自动切换间隙， 单位为毫秒
+//轮播图部分
+const jsImgs_box = document.querySelector('.box-imgs');
+const JSImgs_ul = document.querySelector('.imgs');
+const jsImg = JSImgs_ul.children;
+const jsCirs_ul = document.querySelector('.box-cirs');
+const jsCir = jsCirs_ul.children;
+const jsText = document.querySelectorAll('.box-text');
+// const jsBt = document.querySelector('.bt')
+// console.log(jsCir)
+var len = jsImg.length;//图片的张数
 var timer = null;                     //初始化一个定时器
 var picN = 0;                         //当前显示的图片下标
 var cirN = 0;                         //当前显示图片的小圆点下标
-var temp;
+var textN = 0;                         //当前显示文本的下标
 
-//添加小圆点，之所用js添加小圆点，是因为小圆点的数量是由图片张数决定的。
-for(let i = 0; i < len;i++){
-var a_li = document.createElement('li');//创建li元素节点
-a_li.className = 'quiet';//给li添加类名active
-litCir_ul.appendChild(a_li);
+
+///添加小圆点，之所用js添加小圆点，是因为小圆点的数量是由图片张数决定的。
+for (var i = 0; i < len; i++) {
+    var c_li = document.createElement('li');
+    jsCirs_ul.appendChild(c_li);
 }
-litCir_ul.children[0].className = 'active';
-//通过传进来的distance来控制img_ul的位置来显示      //设置img_ul的left该如何变化，以达到图片轮播的
-function Roll(distance){                                         //参数distance：滚动的目标点（必为图片宽度的倍数）
-    clearInterval(img_ul.timer);                                     //每次运行该函数必须清除之前的定时器！
-    var speed = img_ul.offsetLeft < distance ?  rate : (0-rate);     //判断图片移动的方向  此处用了三元运算符  ？ 前面的不等式成立时为rate,不成立时为0-rete
-
-    img_ul.timer = setInterval(function(){                           //设置定时器，每隔10毫秒，调用一次该匿名函数
-        img_ul.style.left = img_ul.offsetLeft + speed + "px";        //每一次调用滚动到的地方 (速度为 speed px/10 ms)       offsetLeft为元素边框外侧到父元素边框内侧的距离    
-        var leave = distance - img_ul.offsetLeft;                    //距目标点剩余的px值      
-        /*接近目标点时的处理，滚动接近目标时直接到达， 避免rate值设置不当时不能完整显示图片*/
-        // Math.abs()  abs() 方法可返回一个数的绝对值
-        if (Math.abs(leave) <= Math.abs(speed)) {                    
-            clearInterval(img_ul.timer);
-            img_ul.style.left = distance + "px";
-        }
-    },10);
+//定义一个函数，通过传进来的要显示的图片的下标来控制显示
+function Roll(index, num, tn) {
+    for (var i = 0; i < len; i++) {
+        jsImg[i].style.display = 'none';
+        jsText[i].style.display = 'none';
+        jsCir[i].className = '';
     }
-//当图片从最后一张切换到第一张时，这时就不能通过逐渐改变img_ul的left值来实现滚动的效果，于是克隆第一张图片至列表尾部，当滚动完最后一张图片时，继续滚动到克隆的第一张，然后将img_ul的left值置为0。
-    img_ul.appendChild(img_ul.children[0].cloneNode(true));//克隆第一张图片至列表尾部
-    //自动轮播
-    function autoRun(){
-        picN++
-        cirN++
-        //如果轮播完克隆项因该轮播回第二张照片上，因为克隆项和第一张图片一样
-        if(picN > len) {
-            img_ul.style.left = 0;//改变left至真正的第一项处，这个过程是时间太快太短所以可以忽略不计然后立刻
-            picN = 1;   //从第二张开始显示
-        }
-        // 自动轮播，当图片为第一张时应该自动到第二张上去，所以要传入第二张的picN值，以次类推
-        Roll(-picN*width)
-        //判断是否到了最后一个圆点，当圆点到了最后一个时，应该变回第一个点进行轮播
-        if (cirN > len - 1) {
-            cirN = 0;
-        }
-        for(var i = 0; i < len;i++) {
-            cLis[i].className = 'quiet';//让所有圆点背景色变为默认色
-        }
-        cLis[cirN].className = 'active';
-    }
-    //注意：以上轮播图片时是当图片索引值picN已经有了，要+1，等待一定时间执行移动到下一张图片的事件
-    // 开始自动滚动：
-    timer = setInterval(autoRun, gap); //定时器
+    jsImg[index].style.display = 'block';
+    jsCir[num].className = 'active';
+    jsImg[index].className = 'ani';
+    jsText[tn].style.display = 'block';
+    jsText[tn].className = 'showtext';
 
-    //
-    for(let j = 0;j < len ;j++){
-        cLis[j].index = j;//给每个圆点一个索引值
-        cLis[j].onmouseover = function() {
-            for(var k = 0; k < len;k++) {
-                cLis[k].className = 'quiet';//让所有圆点背景色变为默认色
+}
+//设置自动播放函数autoRun(),只要传入当前的图片和圆点的索引值就会从此处开始自动轮播
+function autoRun(pic, cir, t) {
+    // 当调用autoRun（）函数，传入的实参不是pic = 0,cir = 0;时,执行下面语句
+    if (pic != 0) {
+        //当点击最后一个圆点时，要先显示最后一张图片，再从第一张重新开始轮播
+        if(pic === len - 1){
+            Roll(pic, cir, t);
+            pic = 0;
+            cir = 0;
+            t = 0;
+            jsImg.timer = setInterval(function () {
+                Roll(pic, cir, t);
+                pic++;
+                cir++;
+                t++;
+                if (pic > len - 1) {
+                    pic = 0;
+                }
+                if (cir > len - 1) {
+                    cir = 0;
+                }
+                if(t > len - 1) {
+                    t = 0;
+                }
+            }, 7200)
+        }else{//当点击第二、三个圆点时，显示当前点击圆点的图片和高亮，再进行循环
+            Roll(pic, cir, t);
+            ++pic;
+            ++cir;
+            ++t;
+            jsImg.timer = setInterval(function () {
+                Roll(pic, cir, t);
+                pic++;
+                cir++;
+                t++;
+                if (pic > len - 1) {
+                    pic = 0;
+                }
+                if (cir > len - 1) {
+                    cir = 0;
+                }
+                if(t > len - 1) {
+                    t = 0;
+                }
+            }, 7200)
+        }
+       
+    } else{//当页面加载时，预先定义好的pic = 0,cir = 0实参会传进来，执行下列语句，会自动轮播
+        Roll(pic, cir, t);//先显示传入实参的图片和圆点的高亮
+        ++pic
+        ++cir
+        ++t
+        jsImg.timer = setInterval(function () {
+            Roll(pic, cir, t);
+            pic++;
+            cir++;
+            t++;
+            if (pic > len - 1) {
+                pic = 0;
             }
-            this.className = 'active';
-            temp = cirN;
-            picN = cirN = this.index;
-            times = Math.abs(this.index - temp);//距离上个小圆点的距离
-            rate = rate*times;//根据距离改变切换速率
-            Roll(-this.index * width);
-            rate = 50;
-        }
+            if (cir > len - 1) {
+                cir = 0;
+            }
+            if(t > len - 1) {
+                t = 0;
+            }
+        }, 7200)
     }
-    //触及轮播图区域,清除定时器
-    parent.onmouseover = function(){
-        clearInterval(timer);
-    }//离开该区域时，重新开始定时器的自动轮播
-    parent.onmouseout = function(){
-        timer = setInterval(autoRun, gap);
+}
+//当页面加载时就调用autoRun函数自动播放，传入预先定义好的pic = 0,cir = 0实参
+window.onload = autoRun(picN, cirN, textN);
+//用for循环给每个圆点绑定一个点击事件
+for (var j = 0; j < len; j++) {
+    jsCir[j].index = j;//给每个圆点一个索引值
+    //当触及小圆点是高亮
+    jsCir[j].onclick = function () {
+        clearInterval(jsImg.timer)//每次运行该函数必须清除之前的定时器！
+        this.className = 'active';//给当前点击的圆点高亮
+        var i = this.index;  //将当前圆点的index索引值传给i
+        var e = this.index;
+        autoRun(i, this.index, e)  //传入实参i，this.index，调用自动轮播函数autoRun()
     }
+}
+
 
